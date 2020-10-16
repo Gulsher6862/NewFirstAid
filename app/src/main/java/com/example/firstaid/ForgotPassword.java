34156048f1dce497a9314b0139137582f6454ccc
dilpreetbhogal20@gmail.com <dilpreetbhogal20@gmail.com>
2020-10-16 00:34:36 -0700

    Forgot password link with auth complete

--- app/src/main/java/com/example/firstaid/ForgotPassword.java
+++ app/src/main/java/com/example/firstaid/ForgotPassword.java
@@ -31,10 +31,33 @@ public class ForgotPassword extends AppCompatActivity {
         fpb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                String user_email = email.getText().toString().trim();
+                resetLink(user_email);
             }
         });
     }
 
+    private void resetLink(String user_email) {
+        if (user_email.equals("")){
+            Toast.makeText(ForgotPassword.this,"Fields can't be empty!",Toast.LENGTH_LONG).show();
+        }
+        else {
+            fpb.setEnabled(false);
+            mAuth.sendPasswordResetEmail(user_email)
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                fpb.setEnabled(true);
+                                Toast.makeText(ForgotPassword.this, "Link sent!", Toast.LENGTH_LONG).show();
+                                finish();
+                            } else {
+                                fpb.setEnabled(true);
+                                Toast.makeText(ForgotPassword.this, "Email not found!", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
 
 }
